5f11b9525352155c5aa97a943a2ae47d576801be
Paul Ybarra <ybarrap@earthlink.net>
2025-05-18 20:26:43 -0500

    Add files via upload
    PWA initial commit

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('clue-sheet-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
